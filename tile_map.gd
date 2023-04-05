f99431801c977b27a9323c7867534dc69702d158
Kai Liao <8lfkai@gmail.com>
2023-04-04 20:39:10 -0600

    initial random gen plant implementation

--- tile_map.gd
+++ tile_map.gd
@@ -1,8 +1,13 @@
 extends TileMap
 
+const MAX_PLANTS_PER_CELL = 3
+
 @export var map_radius = 3
 
 var player_occupied_cell = null
+var plant_database = {}
+
+var plant_scene = preload("res://plant.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,7 +28,22 @@ func _on_player_moved(player_position):
 		for cell in get_used_cells(0):
 			if cell not in mapped_cells:
 				erase_cell(0, cell)
+				if plant_database.has(cell):
+					while not plant_database[cell].is_empty():
+						plant_database[cell].pop_back().queue_free()
+					plant_database.erase(cell)
 			else:
 				mapped_cells.erase(cell)
 		for cell in mapped_cells:
-			set_cell(0, cell, 1, Vector2i(0, randi_range(0, 1)))
+			var tile_used = randi_range(0, 1)
+			set_cell(0, cell, 1, Vector2i(0, tile_used))
+			var plants_in_cell = randi_range(0, MAX_PLANTS_PER_CELL) if tile_used == 1 else 0
+			for plant in plants_in_cell:
+				var new_plant = plant_scene.instantiate()
+				var cell_position = map_to_local(cell)
+				new_plant.position = Vector2(cell_position.x + randf_range(-200, 200), cell_position.y + randf_range(-75, 75))
+				if plant_database.has(cell):
+					plant_database[cell].append(new_plant)
+				else:
+					plant_database[cell] = [new_plant]
+				add_child(new_plant)
